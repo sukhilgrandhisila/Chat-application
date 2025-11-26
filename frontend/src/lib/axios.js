13566455afc810cb23e836a60f734570d1bedffc
Sukhil <sukhilgrandhisila@gmail.com>
2025-11-26 12:10:57 +0530

    frontend axios URL changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-c8zl.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-c8zl.onrender.com/api" : "/api",
   withCredentials: true,
 });
